a539d7b69a706003ddbdb893a81b136b4da311b5
bsantana17 <bastian.santana@usach.cl>
2017-09-21 14:00:29 -0300

    Añadido crossOrigin

--- src/main/java/cl/citiaps/spring/backend/rest/StudentService.java
+++ src/main/java/cl/citiaps/spring/backend/rest/StudentService.java
@@ -2,6 +2,7 @@ package cl.citiaps.spring.backend.rest;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 import cl.citiaps.spring.backend.entities.Student;
 import cl.citiaps.spring.backend.repository.StudentRepository;
 
+@CrossOrigin(origins = "http://localhost:8000")
 @RestController  
 @RequestMapping("/students")
 public class StudentService {
